e935fe7459622775b0f6cba1d910c0a6b72fda31
Alsjoza <aljosaaka98@gmail.com>
2017-06-14 15:51:13 +0200

    dugmici

---
+++ src/components/Buttons.vue
@@ -0,0 +1,33 @@
+<template>
+    <div >
+        <a class="button dugme  is-primary is-active"  @click="loadCity"><slot></slot></a>
+    </div>
+
+
+</template>
+
+
+<script>
+    export default{
+        props:['cityCode'],
+        methods:{
+            loadCity(){
+                Event.$emit('load-city', { city: this.cityCode })
+            }
+        }
+    }
+
+
+</script>
+
+
+<style>
+
+.dugme{
+    width:200px;
+    margin-left: 10px;
+    float: left;
+}
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+window.axios=axios
+
+window.Event=new Vue()
 
 new Vue({
   el: '#app',
